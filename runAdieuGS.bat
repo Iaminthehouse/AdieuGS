faec4448f4e2bf1cb9f532bad896b18554f68202
FransoaVirtuality <122210992+fransoavirtuality@users.noreply.github.com>
2025-02-07 10:15:50 +0100

    model as paramter

--- runAdieuGS.bat
+++ runAdieuGS.bat
@@ -1,10 +1,22 @@
 set myroot=%CD%
 echo working directory is %myroot%
+
+echo off
+REM Dieses Skript ruft rembg p mit einem Modellnamen auf. 
+REM Wird kein Modellname als Parameter übergeben, wird "birefnet-general" verwendet.
+
+IF "%~1"=="" (
+    SET model=birefnet-general
+) ELSE (
+    SET model=%~1
+)
+
 echo starting background removal...
 cd ./ext/rmbg
 @echo off
 call myenv\Scripts\activate
-rembg p -m birefnet-portrait ../../input ../../output/images
+REM rembg p -m birefnet-portrait ../../input ../../output/images
+rembg p -m %model% ../../input ../../output/images
 call myenv\Scripts\deactivate.bat
 cd %myroot%
 echo starting normals creation...
